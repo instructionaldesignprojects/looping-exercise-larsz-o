e544ee74f86f0b2a78d86e45f7a5fd6e06cf75ea
lars mackenzie <larsmackenzie@gmail.com>
2019-04-04 09:25:29 -0400

    completed assignment

--- example.js
+++ example.js
@@ -1,5 +1,11 @@
 function helloWorld(){
-    console.log('Hello world');
+    // create a loop to log the message as many times as my age 
+    for( x = 0; x++; x < 31 ){
+        console.log('Hello world');
+    }
 }
 
 // to do: log the statement to the console x number of times, where x = your age
+
+// called the function 
+helloWorld();
